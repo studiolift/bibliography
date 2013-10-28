8477e634f4654a174ccad1375a59bb4a9e14991e
Mike Robinson <mike@studiolift.com>
2013-10-28 16:22:35 +0000

    Add BIC subjects CSV and load into Subject class

--- lib/bibliography/bic/subject.rb
+++ lib/bibliography/bic/subject.rb
@@ -1,7 +1,12 @@
+require 'csv'
+
 module Bibliography
   module BIC
     class Subject
-
+      Data = {}
+      CSV.foreach(File.join(File.dirname(__FILE__), 'data', 'subjects.csv')) do |line|
+        Data[line[0]] = line[1]
+      end
     end
   end
 end
