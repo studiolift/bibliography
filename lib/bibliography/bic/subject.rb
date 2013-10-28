d02cb43d139b3725fe5bc100666556873439456d
Mike Robinson <mike@studiolift.com>
2013-10-28 17:18:42 +0000

    Methods for traversal to parent and child subjects

--- lib/bibliography/bic/subject.rb
+++ lib/bibliography/bic/subject.rb
@@ -26,6 +26,26 @@ module Bibliography
       def valid?
         !@data.empty?
       end
+
+      def children?
+        !children.empty?
+      end
+
+      def children
+        return [] unless valid?
+        len = code.length
+        @children ||= Data.keys.select{ |key|
+          key.length > len && key[0..len-1] == code
+        }.map {|s| Subject.new(s) }
+      end
+
+      def parent?
+        !parent.nil? && parent.valid?
+      end
+
+      def parent
+        @parent ||= Subject.new(code[0]) if valid? && code.length > 1
+      end
     end
   end
 end
--- spec/bibliography/bic/subject_spec.rb
+++ spec/bibliography/bic/subject_spec.rb
@@ -18,7 +18,31 @@ describe Bibliography::BIC::Subject do
 
     describe ".valid?" do
       it "is true" do
-        expect(subject.valid?).to eq(true)
+        expect(subject.valid?).to be_true
+      end
+    end
+
+    describe ".children?" do
+      it "is true" do
+        expect(subject.children?).to be_true
+      end
+    end
+
+    describe ".children" do
+      it "returns an array of child subjects" do
+        expect(subject.children).to have(4).items
+      end
+    end
+
+    describe ".parent?" do
+      it "is true" do
+        expect(subject.parent?).to be_true
+      end
+    end
+
+    describe ".parent" do
+      it "returns the parent Subject" do
+        expect(subject.parent.code).to eq("A")
       end
     end
   end
@@ -40,7 +64,31 @@ describe Bibliography::BIC::Subject do
 
     describe ".valid?" do
       it "is false" do
-        expect(subject.valid?).to eq(false)
+        expect(subject.valid?).to be_false
+      end
+    end
+
+    describe ".children?" do
+      it "is false" do
+        expect(subject.children?).to be_false
+      end
+    end
+
+    describe ".children" do
+      it "is an empty array" do
+        expect(subject.children).to be_empty
+      end
+    end
+
+    describe ".parent?" do
+      it "is false" do
+        expect(subject.parent?).to be_false
+      end
+    end
+
+    describe ".parent" do
+      it "is nil" do
+        expect(subject.parent).to be_nil
       end
     end
   end
